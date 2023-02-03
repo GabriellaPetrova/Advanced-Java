4cb9d0676ca3b041a42395b9e193e73261818752
GabriellaPetrova <107034894+GabriellaPetrova@users.noreply.github.com>
2023-02-03 14:00:43 +0200

    Add files via upload

---
+++ StreamFilesAndDirectories/FirstFile.java
@@ -0,0 +1,19 @@
+package JavaAdvanced.DefiningClasesExercises.StreamFilesAndDirectories;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FirstFile {
+    public static void main(String[] args) throws IOException {
+        File file = new File("D:\\bobi\\Java\\Advanced\\src\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\Files-and-Streams");
+
+        if (file.exists()) {
+            File[] files = file.listFiles();
+            for (File f : files != null ? files : new File[0]) {
+                if (!f.isDirectory()) {
+                    System.out.println(f.getName() + ": [" + f.length() + "]");
+                }
+            }
+        }
+    }
+}
