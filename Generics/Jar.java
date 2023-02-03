e3d2d11df2e3e84cd67be5399e2612cd7f460366
GabriellaPetrova <107034894+GabriellaPetrova@users.noreply.github.com>
2023-02-03 13:55:33 +0200

    Add files via upload

---
+++ Generics/Jar.java
@@ -0,0 +1,20 @@
+package JavaAdvanced.DefiningClasesExercises.Generics;
+
+import java.util.ArrayDeque;
+
+public class Jar<E> {
+    private ArrayDeque<E> elements;
+
+    public Jar() {
+        this.elements = new ArrayDeque<>();
+    }
+
+    public void add(E element){
+        elements.add(element);
+    }
+
+    public E remove(){
+        return elements.pop();
+    }
+
+}
