8670836d4771a5e97755331733485c58fe54e0ee
GabriellaPetrova <107034894+GabriellaPetrova@users.noreply.github.com>
2023-02-03 13:54:47 +0200

    Add files via upload

---
+++ FunctionalProgrammingExercises/CustomMinFunction.java
@@ -0,0 +1,13 @@
+package JavaAdvanced.DefiningClasesExercises.FunctionalProgrammingExercises;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class CustomMinFunction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Function<Integer[], Integer> maxElement = intArray -> Arrays.stream(intArray).sorted().findFirst().orElse(0);
+        System.out.println(maxElement.apply(Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt).toArray(Integer[]::new)));
+    }
+}
